dd0b330ab58a15c9836b85815acab61d7429ce60
Keks <keks@htmlacademy.ru>
2022-10-02 07:55:36 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -2,7 +2,7 @@
 
 function getRandomIntInclusive(min, max) {
   if (min < 0 || max <= min) {
-    return 0 / 0;
+    return NaN;
   }
 
   min = Math.ceil(min);
@@ -16,10 +16,22 @@ getRandomIntInclusive(0, 10);
 
 function getRandomFloatInt(min, max, amountOfDecimal) {
   if (min < 0 || max <= min) {
-    return 0 / 0;
+    return NaN;
   }
 
   return (Math.random() * (max - min) + min).toFixed(amountOfDecimal);
 }
 
 getRandomFloatInt(0, 10, 3);
+
+// Функция проверки максимальной длины строки
+
+function checkMaximumLength(checkedString, maximalLenght) {
+  if (checkedString.length <= maximalLenght) {
+    return true;
+  }
+
+  return false;
+}
+
+checkMaximumLength('text of some string', 140);
