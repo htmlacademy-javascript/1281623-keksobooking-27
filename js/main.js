7b53d16e7f7eeddc95fdaa5e8472c50a97dac44b
Arty Violet <myartysoul@gmail.com>
2022-10-08 19:39:06 +0300

    Добавлены объекты и функции

--- js/main.js
+++ js/main.js
@@ -1,37 +1,90 @@
 // Получение случайного целого числа в заданном интервале, включительно
 
-function getRandomIntInclusive(min, max) {
-  if (min < 0 || max <= min) {
+function getRandomInteger(a, b) {
+  if (a < 0 || b < 0) {
     return NaN;
   }
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
 }
 
-getRandomIntInclusive(0, 10);
+getRandomInteger(0, 10);
 
-// Получение случайного числа в заданном интервале, включительно
+// Получение случайного нецелого числа в заданном интервале, включительно
 
-function getRandomFloatInt(min, max, amountOfDecimal) {
-  if (min < 0 || max <= min) {
+function getRandomFloat(a, b, amountOfDecimal = 1) {
+  if (a < 0 || b < 0 || amountOfDecimal < 0) {
     return NaN;
   }
 
-  return (Math.random() * (max - min) + min).toFixed(amountOfDecimal);
+  const lower = Math.min(a, b);
+  const upper = Math.max(a, b);
+  const result = Math.random() * (upper - lower) + lower;
+
+  return Number(result.toFixed(amountOfDecimal));
 }
 
-getRandomFloatInt(0, 10, 3);
+getRandomFloat(0, 10, 3);
 
-// Функция проверки максимальной длины строки
+// Получение числа от 01 до 10
 
-function checkMaximumLength(checkedString, maximalLenght) {
-  if (checkedString.length <= maximalLenght) {
-    return true;
+function getNumber(number) {
+  if (number < 1 || number > 10) {
+    return NaN;
   }
 
-  return false;
+  if (number >= 1 && number <= 9) {
+    return `0${number}`;
+  }
+
+  return number;
 }
 
-checkMaximumLength('text of some string', 140);
+const AMOUNT_OF_OBJECTS = 10;
+
+const xx = getNumber(1);
+
+const author = {
+  avatar: `img/avatars/user${xx}.png`,
+};
+
+const location1 = {
+  lat: getRandomFloat(35.65, 35.7, 5),
+  lng: getRandomFloat(139.7, 139.8, 5),
+};
+
+const offer = {
+  title: 'Квартира с самым красивым видом из окна',
+  address: `${location1.lat}, ${location1.lng}`,
+  price: 3000,
+  type: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+  rooms: 2,
+  guests: 3,
+  checkin: ['12:00', '13:00', '14:00'],
+  checkout: ['12:00', '13:00', '14:00'],
+  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  description: 'Квартира расположена в самом центре города с видом на парк. Квартира оснащена кондиционером, письменным столом, сейфом, телевизором с плоским экраном. Парам особенно нравится расположение — они оценили проживание в этом районе для поездки вдвоем',
+  photos: [
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/// keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// claire-rendall-b6kAwr1i0Iw.jpg',
+  ],
+};
+
+// Создание объекта с описанием объявления
+
+function createPublication() {
+  return {
+    author,
+    offer,
+  };
+}
+
+// Создание объектов на основе созданного объявления
+
+const publications = Array.from({ length: AMOUNT_OF_OBJECTS }, createPublication);
+
