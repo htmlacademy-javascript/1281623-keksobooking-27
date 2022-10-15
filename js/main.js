cf30424d2bdb3751d40fc50ddecd436617dfb5c6
Arty Violet <myartysoul@gmail.com>
2022-10-15 17:21:44 +0300

    Выведены массивы в отдельные константы

--- js/main.js
+++ js/main.js
@@ -32,11 +32,11 @@ function getRandomArray(features) {
   const array = [];
 
   while (array.length < lengthOfArray) {
-    const indexOfEl = getRandomInteger(0, maxLength - 1);
-    const el = features[indexOfEl];
+    const indexOfArray = getRandomInteger(0, maxLength - 1);
+    const element = features[indexOfArray];
 
-    if (!array.includes(el)) {
-      array.push(el);
+    if (!array.includes(element)) {
+      array.push(element);
     }
   }
   return array;
@@ -55,6 +55,71 @@ function getRandomArray(features) {
 //   };
 // }
 
+const TITLES = [
+  'Квартира 1',
+  'Квартира 2',
+  'Квартира 3',
+  'Квартира 4',
+  'Квартира 5',
+  'Квартира 6',
+  'Квартира 7',
+  'Квартира 8',
+  'Квартира 9',
+  'Квартира 10',
+];
+
+const MIN_PRICE = 1500;
+
+const MAX_PRICE = 3000;
+
+const TYPES_OF_ROOMS = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES_OF_ROOMS = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const DESCRIPTIONS = [
+  'Описание 1',
+  'Описание 2',
+  'Описание 3',
+  'Описание 4',
+  'Описание 5',
+  'Описание 6',
+  'Описание 7',
+  'Описание 8',
+  'Описание 9',
+  'Описание 10',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/// keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
 const OFFERS_COUNT = 10;
 
 function createOffer(_, index) {
@@ -68,43 +133,17 @@ function createOffer(_, index) {
   return {
     author: { avatar: `img/avatars/user${avatarId}.png` },
     offer: {
-      title: getRandomArrayElement([
-        'Квартира 1',
-        'Квартира 2',
-        'Квартира 3',
-        'Квартира 4',
-        'Квартира 5',
-        'Квартира 6',
-        'Квартира 7',
-        'Квартира 8',
-        'Квартира 9',
-        'Квартира 10',
-      ]),
+      title: getRandomArrayElement(TITLES),
       address: `${location.lat}, ${location.lng}`,
-      price: Math.round(getRandomInteger(1500, 3000) / 100) * 100,
-      type: getRandomArrayElement(['palace', 'flat', 'house', 'bungalow', 'hotel']),
+      price: Math.round(getRandomInteger(MIN_PRICE, MAX_PRICE) / 100) * 100,
+      type: getRandomArrayElement(TYPES_OF_ROOMS),
       rooms: getRandomInteger(1, 3),
       guests: getRandomInteger(1, 6),
-      checkin: getRandomArrayElement(['12:00', '13:00', '14:00']),
-      checkout: getRandomArrayElement(['11:00', '12:00', '13:00']),
-      features: getRandomArray(['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']),
-      description: getRandomArrayElement([
-        'Описание 1',
-        'Описание 2',
-        'Описание 3',
-        'Описание 4',
-        'Описание 5',
-        'Описание 6',
-        'Описание 7',
-        'Описание 8',
-        'Описание 9',
-        'Описание 10',
-      ]),
-      photos: getRandomArrayElement([
-        'https://assets.htmlacademy.ru/content/intensive/javascript-1/// keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// brandon-hoogenboom-SNxQGWxZQi0.jpg',
-        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// claire-rendall-b6kAwr1i0Iw.jpg',
-      ]),
+      checkin: getRandomArrayElement(CHECKINS),
+      checkout: getRandomArrayElement(CHECKOUTS),
+      features: getRandomArray(FEATURES_OF_ROOMS),
+      description: getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomArrayElement(PHOTOS),
     },
     location
   };
