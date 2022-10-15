e68efc0734f7e8beec17d2eb6cd2fc52b7ae736f
Arty Violet <myartysoul@gmail.com>
2022-10-15 12:36:39 +0300

    Доработана функция createOffer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-inner-declarations */
-// Получение случайного целого числа в заданном интервале, включительно
-
 function getRandomInteger(a, b) {
   if (a < 0 || b < 0) {
     return NaN;
@@ -12,10 +9,6 @@ function getRandomInteger(a, b) {
 
   return Math.floor(result);
 }
-
-getRandomInteger(0, 10);
-
-// Получение случайного нецелого числа в заданном интервале, включительно
 
 function getRandomFloat(a, b, amountOfDecimal = 1) {
   if (a < 0 || b < 0 || amountOfDecimal < 0) {
@@ -29,77 +22,95 @@ function getRandomFloat(a, b, amountOfDecimal = 1) {
   return Number(result.toFixed(amountOfDecimal));
 }
 
-getRandomFloat(0, 10, 3);
-
-// Получение числа от 01 до 10
-
-// function getNumber(number) {
-//   if (number < 1 || number > 10) {
-//     return NaN;
-//   }
-
-//   if (number >= 1 && number <= 9) {
-//     return `0${number}`;
-//   }
-
-//   return number;
-// }
-
-const AMOUNT_OF_OBJECTS = 10;
-
-function createIdGenerator () {
-  let lastGeneratedId = 0;
-
-  return function () {
-    lastGeneratedId += 1;
-    if (lastGeneratedId >= 1 && lastGeneratedId <= 9) {
-      return `0${lastGeneratedId}`;
-    }
-
-    return lastGeneratedId;
-  };
+function getRandomArrayElement(array) {
+  return array[getRandomInteger(0, array.length - 1)];
 }
 
-const xx = createIdGenerator();
+function getRandomArray(features) {
+  const maxLength = features.length;
+  const lengthOfArray = getRandomInteger(1, maxLength);
+  const array = [];
 
-// const author = {
-//   avatar: `img/avatars/user${xx}.png`,
-// };
+  while (array.length < lengthOfArray) {
+    const indexOfEl = getRandomInteger(0, maxLength - 1);
+    const el = features[indexOfEl];
 
-{
+    if (!array.includes(el)) {
+      array.push(el);
+    }
+  }
+  return array;
+}
+
+// function createIdGenerator () {
+//   let lastGeneratedId = 0;
+
+//   return function () {
+//     lastGeneratedId += 1;
+//     if (lastGeneratedId >= 1 && lastGeneratedId <= 9) {
+//       return `0${lastGeneratedId}`;
+//     }
+
+//     return lastGeneratedId;
+//   };
+// }
+
+const OFFERS_COUNT = 10;
+
+function createOffer(_, index) {
   const location = {
     lat: getRandomFloat(35.65, 35.7, 5),
     lng: getRandomFloat(139.7, 139.8, 5),
   };
 
-  const offer = {
-    title: 'Квартира с самым красивым видом из окна',
-    address: `${location.lat}, ${location.lng}`,
-    price: 3000,
-    type: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
-    rooms: 2,
-    guests: 3,
-    checkin: ['12:00', '13:00', '14:00'],
-    checkout: ['12:00', '13:00', '14:00'],
-    features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-    description: 'Квартира расположена в самом центре города с видом на парк. Квартира оснащена кондиционером, письменным столом, сейфом, телевизором с плоским экраном. Парам особенно нравится расположение — они оценили проживание в этом районе для поездки вдвоем',
-    photos: [
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/// keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// brandon-hoogenboom-SNxQGWxZQi0.jpg',
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// claire-rendall-b6kAwr1i0Iw.jpg',
-    ],
+  const avatarId = `${index + 1}`.padStart(2, '0');
+
+  return {
+    author: { avatar: `img/avatars/user${avatarId}.png` },
+    offer: {
+      title: getRandomArrayElement([
+        'Квартира 1',
+        'Квартира 2',
+        'Квартира 3',
+        'Квартира 4',
+        'Квартира 5',
+        'Квартира 6',
+        'Квартира 7',
+        'Квартира 8',
+        'Квартира 9',
+        'Квартира 10',
+      ]),
+      address: `${location.lat}, ${location.lng}`,
+      price: Math.round(getRandomInteger(1500, 3000) / 100) * 100,
+      type: getRandomArrayElement(['palace', 'flat', 'house', 'bungalow', 'hotel']),
+      rooms: getRandomInteger(1, 3),
+      guests: getRandomInteger(1, 6),
+      checkin: getRandomArrayElement(['12:00', '13:00', '14:00']),
+      checkout: getRandomArrayElement(['11:00', '12:00', '13:00']),
+      features: getRandomArray(['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']),
+      description: getRandomArrayElement([
+        'Описание 1',
+        'Описание 2',
+        'Описание 3',
+        'Описание 4',
+        'Описание 5',
+        'Описание 6',
+        'Описание 7',
+        'Описание 8',
+        'Описание 9',
+        'Описание 10',
+      ]),
+      photos: getRandomArrayElement([
+        'https://assets.htmlacademy.ru/content/intensive/javascript-1/// keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// brandon-hoogenboom-SNxQGWxZQi0.jpg',
+        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/// claire-rendall-b6kAwr1i0Iw.jpg',
+      ]),
+    },
+    location
   };
-
-  // Создание объекта с описанием объявления
-
-  function createPublication() {
-    return {
-      author: { avatar: `img/avatars/user${xx()}.png` },
-      offer,
-    };
-  }
-
-  // Создание объектов на основе созданного объявления
-
-  Array.from({ length: AMOUNT_OF_OBJECTS }, createPublication);
 }
+
+const offers = Array.from({ length: OFFERS_COUNT }, createOffer);
+
+// eslint-disable-next-line no-console
+console.log(offers);
