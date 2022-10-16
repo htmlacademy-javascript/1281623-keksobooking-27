f8d38a699387ab12b67e4f8212a147c2c935911b
Keks <keks@htmlacademy.ru>
2022-10-16 15:15:15 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,8 @@
+import './util.js';
+import {offers} from './data.js';
+import './validation.js';
+import './map.js';
+import './filter.js';
+
+// eslint-disable-next-line no-console
+console.log(offers);
