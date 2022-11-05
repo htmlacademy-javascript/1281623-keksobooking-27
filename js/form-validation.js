0e46be450fb41b9c4d3b928a76f38bce273ed1e4
Arty Violet <myartysoul@gmail.com>
2022-11-05 20:22:56 +0300

    Добавлена валидация дополнительным полям формы

--- js/form-validation.js
+++ js/form-validation.js
@@ -8,6 +8,8 @@ const pristine = new Pristine(adForm, {
   errorTextClass: 'text-help',
 });
 
+// Валидация на соответствие количества комнат и количества гостей
+
 const roomNumber = adForm.querySelector('[name="rooms"]');
 const capacity = adForm.querySelector('[name="capacity"]');
 const roomNumberOption = {
@@ -32,6 +34,49 @@ function getRoomNumberErrorMessage () {
 pristine.addValidator(roomNumber, validateRoomNumber, getRoomNumberErrorMessage);
 pristine.addValidator(capacity, validateRoomNumber, getRoomNumberErrorMessage);
 
+// Валидация на соответствие типа жилья и минимальной цены за ночь
+
+const housingType = adForm.querySelector('[name="type"]');
+const priceField = adForm.querySelector('[name="price"]');
+const minPrice = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
+
+function validatePrice (value) {
+  return value.length && parseInt(value, 10) >= minPrice[housingType.value];
+}
+
+function getPriceErrorMessage () {
+  return `Минимальная цена ${minPrice[housingType.value]}`;
+}
+
+function onHousingTypeChange() {
+  priceField.placeholder = minPrice[this.value];
+  pristine.validate(priceField.placeholder);
+}
+
+const housingTypes = adForm.querySelectorAll('[name="type"]');
+
+housingTypes.forEach((item) => item.addEventListener('change', onHousingTypeChange));
+
+pristine.addValidator(priceField, validatePrice, getPriceErrorMessage);
+
+// Синхронизация времени заезда и выезда
+
+const adFormTime = adForm.querySelector('.ad-form__element--time');
+const timeIn = adForm.querySelector('[name="timein"]');
+const timeOut = adForm.querySelector('[name="timeout"]');
+
+adFormTime.addEventListener('change', (evt) => {
+  timeIn.value = evt.target.value;
+  timeOut.value = evt.target.value;
+});
+
+// Запуск валидации при отправке формы
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
